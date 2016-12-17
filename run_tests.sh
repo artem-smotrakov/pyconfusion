4b22057d1404262bbee624f5db0b13941db0d45a
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-12-16 22:47:04 -0800

    Updated run_test.sh to pass options

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 PYTHON=${1:-"python"}
 TESTS=${2:-"./"}
+OPTIONS=${3:-""}
 
 # disable memory leaks checker
 export ASAN_OPTIONS="${ASAN_OPTIONS} detect_leaks=0"
@@ -9,7 +10,7 @@ export ASAN_OPTIONS="${ASAN_OPTIONS} detect_leaks=0"
 for TEST in `ls ${TESTS}`
 do
     echo "run ${TEST}"
-    ${PYTHON} ${TEST} > log 2>&1
+    ${PYTHON} ${OPTIONS} ${TEST} > log 2>&1
 
     if grep "AddressSanitizer" log > /dev/null 2>&1; then
         echo "ASan found something, game over"
