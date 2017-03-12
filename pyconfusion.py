20b408ac55dee952c8ac7c5a0b6c8ac1bea0750c
Artem Smotrakov <artem.smotrakov@gmail.com>
2017-03-12 11:47:33 -0700

    Updated commands, removed --finder option

--- pyconfusion.py
+++ pyconfusion.py
@@ -19,31 +19,33 @@ class Task:
         if self.args['exclude']:
             self.excludes = self.args['exclude'].split(',')
 
-    def __getattr__(self, name):
-        return self.args[name]
+    def command(self):  return self.args['command']
+    def src(self):      return self.args['src']
+    def no_src(self):   return self.src() == None
 
     def run(self):
-        if self.args['command'] == 'targets':
-            self.search_targets()
-        elif self.args['command'] == 'fuzzer':
-            targets = self.search_targets()
-            self.fuzz(targets)
+        if self.no_src() == None: raise Exception('Sources not specified')
+        if self.command() == 'clinic_targets':  self.search_clinic_targets()
+        elif self.command() == 'c_targets':     self.search_c_targets()
+        elif self.command() == 'clinic_fuzzer': self.fuzz_clinic()
+        elif self.command() == 'c_fuzzer':      self.fuzz_c()
         else:
-            raise Exception('Unknown command: ' + self.args['command'])
+            raise Exception('Unknown command: ' + self.command())
 
-    def search_targets(self):
-        if self.args['src'] == None:
-            raise Exception('Sources not specified')
-        if self.args['finder'] == 'clinic':
-            finder = ClinicTargetFinder(self.args['src'])
-        elif self.args['finder'] == 'c':
-            finder = CTargetFinder(self.args['src'])
-        else:
-            raise Exception('Unexpected finder type: ' + self.args['finder'])
-        return finder.run(self.args['finder_filter'])
+    def search_clinic_targets(self):
+        return ClinicTargetFinder(self.src()).run(self.args['finder_filter'])
 
-    def fuzz(self, targets):
-        for target in targets:
+    def search_c_targets(self):
+        CTargetFinder(self.src()).run(self.args['finder_filter'])
+
+    def fuzz_clinic(self):
+        self.fuzz(self.search_clinic_targets())
+
+    def fuzz_c(self):
+        raise Exception('not implemented yet')
+
+    def fuzz_clinic(self, targets):
+        for target in self.search_clinic_targets():
             # check if the line matches specified filter
             if self.skip(target): continue
 
@@ -71,8 +73,9 @@ class Task:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--src',  help='path to sources', default='./')
-parser.add_argument('--command', help='what do you want to do?', choices=['targets', 'fuzzer'], default='targets')
-parser.add_argument('--finder', help='type of parser of C files', choices=['clinic', 'c'], default='clinic')
+parser.add_argument('--command', help='what do you want to do?',
+                    choices=['clinic_targets', 'clinic_fuzzer', 'c_targets', 'c_fuzzer'],
+                    default='c_targets')
 parser.add_argument('--mode', help='how do you want to do?', choices=['light', 'hard'], default='light')
 parser.add_argument('--fuzzer_filter',  help='target filter for fuzzer', default='')
 parser.add_argument('--finder_filter',  help='file filter for finder', default='')
