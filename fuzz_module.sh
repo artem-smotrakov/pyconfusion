5ec84c64f781d9c4a9498a4355faf02a5b73facd
Artem Smotrakov <artem.smotrakov@gmail.com>
2017-04-16 12:07:48 -0700

    Removed redundant scripts

--- fuzz_module.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-EXCLUDE_LIST=${EXCLUDE_LIST:-"_testcapi"}
-ASAN_OPTIONS="detect_leaks=0" \
-    ${PYTHON} \
-        pyconfusion.py \
-            --command fuzzer \
-            --exclude ${EXCLUDE_LIST} \
-            --modules ${MODULES} \
